358bddd6c08529f8451150f50e90c60d81d7dc94
SackOfParticles <52044111+SackOfParticles@users.noreply.github.com>
2019-06-20 21:42:19 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Flowers> flowersList = new ArrayList<Flowers>();
+        flowersList.add(flower1, 500.00);
+        flowersList.add(flower2, 500.00);
+        flowersList.add(flower3, 500.00);
+        flowersList.add(flower4, 500.00);
+        flowersList.add(flower5, 500.00);
+        flowersList.add(flower6, 500.00);
+        System.out.println();
+    }
+}
